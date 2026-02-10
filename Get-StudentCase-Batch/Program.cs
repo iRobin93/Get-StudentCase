5a6c807bc20995c1dfa403ef60b1f5b372f67650
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2026-02-10 11:20:30 +0100

    updates

--- Get-StudentCase-Batch/Program.cs
+++ Get-StudentCase-Batch/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+
+var options = new JsonSerializerOptions
+{
+    PropertyNameCaseInsensitive = true
+};
+
+using var http = new HttpClient
+{
+    BaseAddress = new Uri("https://localhost:7014")
+};
+
+http.DefaultRequestHeaders.Accept.Add(
+    new MediaTypeWithQualityHeaderValue("application/json"));
+
+foreach (var line in File.ReadLines("events.jsonl"))
+{
+    if (string.IsNullOrWhiteSpace(line))
+        continue;
+
+    var evt = JsonSerializer.Deserialize<JsonElement>(line, options);
+
+    var type = evt.GetProperty("type").GetString();
+    var eventId = evt.GetProperty("eventId").GetString();
+
+
+    // TODO: route to handlers, insert into DB, etc
+    if(type == "student_registrert")
+    {
+
+
+        using var content = new StringContent(
+            line,
+            Encoding.UTF8,
+            "application/json");
+
+        var response = await http.PostAsync("/api/events", content);
+        if (!response.IsSuccessStatusCode)
+        {
+            var error = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"❌ Failed: {response.StatusCode} - {error}");
+        }
+    }
+}
