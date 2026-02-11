697ab2827488dfa17ba56ba4ef909eeb4ff0022c
iRobin93 <61976533+iRobin93@users.noreply.github.com>
2026-02-11 11:26:48 +0100

    updates

---
+++ Get-StudentCase-Batch/CreateEventResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Get_StudentCase_Batch
+{
+    public class CreateEventResponseDTO
+    {
+        public string StudentId { get; set; }
+    }
+}
--- Get-StudentCase-Batch/Program.cs
+++ Get-StudentCase-Batch/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System.Net.Http.Headers;
+using System.Net.Http.Json;
 using System.Text;
 using System.Text.Json;
+using System.Text.Json.Nodes;
+using Get_StudentCase_Batch;
 
 var options = new JsonSerializerOptions
 {
@@ -24,23 +27,58 @@ foreach (var line in File.ReadLines("events.jsonl"))
 
     var type = evt.GetProperty("type").GetString();
     var eventId = evt.GetProperty("eventId").GetString();
-
+   
+    string studentId = "";
 
     // TODO: route to handlers, insert into DB, etc
-    if(type == "student_registrert")
+    if (type == "student_registrert")
     {
-
 
         using var content = new StringContent(
             line,
             Encoding.UTF8,
             "application/json");
 
-        var response = await http.PostAsync("/api/events", content);
+        var response = await http.PostAsync("/Event", content);
+        if (!response.IsSuccessStatusCode)
+        {
+            var error = await response.Content.ReadAsStringAsync();
+            Console.WriteLine($"❌ Failed: {response.StatusCode} - {error}");
+            continue;
+        }
+
+        var result = await response.Content.ReadFromJsonAsync<CreateEventResponseDTO>();
+
+      
+        studentId = result!.StudentId;
+
+    }
+    else 
+    {
+
+        var node = JsonNode.Parse(line)!.AsObject();
+
+        node["studentId"] = studentId; // your value
+
+        var lineWithStudentId = node.ToJsonString();
+
+        using var content = new StringContent(
+            lineWithStudentId,
+            Encoding.UTF8,
+            "application/json");
+
+        
+        var response = await http.PostAsync("/Event", content);
         if (!response.IsSuccessStatusCode)
         {
             var error = await response.Content.ReadAsStringAsync();
             Console.WriteLine($"❌ Failed: {response.StatusCode} - {error}");
         }
+
+
     }
 }
+
+
+
+Console.ReadLine();
